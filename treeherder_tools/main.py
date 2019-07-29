796260e486cfdc7c95ed695552330416b653de25
Edwin Gao <egao@outlook.com>
2019-07-29 11:05:26 -0700

    Support list of keywords

--- treeherder_tools/main.py
+++ treeherder_tools/main.py
@@ -1,5 +1,6 @@
 import ast
 import json
+import re
 import requests
 import sys
 import time
@@ -8,20 +9,21 @@ from argparse import ArgumentParser
 
 import thclient
 
-def create_parser(args):
 
+def parse_arguments(argv):
     parser = ArgumentParser()
-    parser.add_argument('--keyword', action='store', default=None, nargs='?', help="Keyword to search in the logs.")
-    parser.add_argument('--revision', action='store', default=None, nargs='1', help="Revision to search against. Obtain from Treeherder.")
-    parser.add_argument('--branch', action='store', default='try', nargs='?', help="Treeherder branch.")
+    parser.add_argument('--keywords', action='store', default=None, required=True, nargs='+', help="One or more keywords to search in the logs.")
+    parser.add_argument('--revision', action='store', default=None, required=True, help="Revision to search against. Obtain from Treeherder.")
+    parser.add_argument('--branch', action='store', default='try', help="Treeherder branch.")
+    parser.add_argument('--exact', action='store_true', default=False, help='Require exact match with the test name.')
 
-    args, _ = parser.parse_known_args()
+    args, _ = parser.parse_known_args(argv)
     return args
 
 def process_logs(args):
     client = thclient.TreeherderClient()
 
-    keyword = args.keyword
+    keywords = args.keywords
 
     pushes = client.get_pushes(
         args.branch, **{"revision": args.revision})
@@ -46,34 +48,44 @@ def process_logs(args):
     api_query_time = round(time.perf_counter() - start_time, 5)
     print(f'Finished querying Treeherder API in {api_query_time} seconds.')
 
+    matches = {keyword: [] for keyword in keywords}
+
+    expression = [(re.compile(r".*{}.*".format(keyword)), keyword) for keyword in args.keywords]
+
     for job_name in logs_for_all_tests:
         for url in logs_for_all_tests[job_name]:
             response = requests.get(url)
             assert response.status_code == 200
             assert response.text
 
-            separated_log = response.text.split('\n')
-            if url.endswith('live_backlog.log'):
-                unexpected_failures = [
-                    line for line in separated_log if 'TEST-UNEXPECTED-FAIL' in line
-                ]
-                match = any(
-                    [True for failure in unexpected_failures if keyword in failure]
-                )
+            separated_log = list(filter(None, response.text.split('\n')))
+            if url.endswith('live_backing.log'):
+                unexpected_failures = [line for line in separated_log if 'TEST-UNEXPECTED-FAIL' in line]
+                for regex, keyword in expression:
+                    if any([regex.match(failure) for failure in unexpected_failures]):
+                        matches[keyword].append(job_name)
             elif url.endswith('errorsummary.log'):
-                match = [
-                    line for line in separated_log if keyword in line
-                ]
+                separated_log = list(map(json.loads, separated_log[1:]))
+                for regex, keyword in expression:
+                    if any([regex.match(failure['test']) for failure in separated_log]):
+                        matches[keyword].append(job_name)
             else:
                 continue
 
-            if match:
-                print('Failure {} found in {}'.format(keyword, job_name))
+    if matches:
+        print('Failures found:')
+        for key in matches.keys():
+            print('-->', key)
+            list(map(print, matches[key]))
+            print('\n')
 
     finish_time = round(time.perf_counter() - api_query_time, 5)
     print(f'Finished processing task logs in {finish_time} seconds.')
 
 
+def main():
+    args = parse_arguments(sys.argv[1:])
+    process_logs(args)
+
 if __name__ == '__main__':
-    create_parser(sys.argv([1:]))
-    process_logs()
+    main()
