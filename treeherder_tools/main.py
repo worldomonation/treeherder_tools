ff05fa2928910369c5b338069c5fdcadf3f8a07c
Edwin Gao <egao@outlook.com>
2019-07-30 10:50:08 -0700

    Add --ran argument to simply parse the log file for the test name.

--- treeherder_tools/main.py
+++ treeherder_tools/main.py
@@ -15,20 +15,20 @@ def parse_arguments(argv):
     parser.add_argument('--keywords', action='store', default=None, required=True, nargs='+', help="One or more keywords to search in the logs.")
     parser.add_argument('--revision', action='store', default=None, required=True, help="Revision to search against. Obtain from Treeherder.")
     parser.add_argument('--branch', action='store', default='try', help="Treeherder branch.")
+    parser.add_argument('--ran', action='store_true', default=False, help='Checks if test was run, and which chunk.')
     parser.add_argument('--exact', action='store_true', default=False, help='Require exact match with the test name.')
 
     args, _ = parser.parse_known_args(argv)
     return args
 
-def process_logs(args):
+
+def get_list_of_log_urls(branch, revision):
     client = thclient.TreeherderClient()
 
-    keywords = args.keywords
-
     pushes = client.get_pushes(
-        args.branch, **{"revision": args.revision})
+        branch, **{"revision": revision})
 
-    logs_for_all_tests = {}
+    log_urls = {}
 
     start_time = time.perf_counter()
 
@@ -42,50 +42,73 @@ def process_logs(args):
 
                 response = client.get_job_log_url('try', **{"job_id": job_id})
 
-                list_of_log_urls = [entry['url'] for entry in response if entry.get('url', None) is not None]
-                logs_for_all_tests[job_name] = list_of_log_urls
+                log_urls[job_name] = [entry['url']
+                            for entry in response if entry.get('url', None) is not None]
 
     api_query_time = round(time.perf_counter() - start_time, 5)
     print(f'Finished querying Treeherder API in {api_query_time} seconds.')
 
+    return log_urls
+
+
+def process_logs(log_urls, keywords, ran):
+    start_time = time.perf_counter()
     matches = {keyword: [] for keyword in keywords}
+    expression = [(re.compile(r".*{}.*".format(keyword)), keyword) for keyword in keywords]
 
-    expression = [(re.compile(r".*{}.*".format(keyword)), keyword) for keyword in args.keywords]
-
-    for job_name in logs_for_all_tests:
-        for url in logs_for_all_tests[job_name]:
+    for job_name in log_urls:
+        for url in log_urls[job_name]:
             response = requests.get(url)
             assert response.status_code == 200
             assert response.text
 
             separated_log = list(filter(None, response.text.split('\n')))
-            if url.endswith('live_backing.log'):
-                unexpected_failures = [line for line in separated_log if 'TEST-UNEXPECTED-FAIL' in line]
+
+            if ran:
+                if not url.endswith('live_backing.log'):
+                    continue
+                tests_ran = [
+                    line for line in separated_log if 'TEST-OK' in line]
                 for regex, keyword in expression:
-                    if any([regex.match(failure) for failure in unexpected_failures]):
+                    if any([regex.match(line) for line in tests_ran]):
                         matches[keyword].append(job_name)
-            elif url.endswith('errorsummary.log'):
-                separated_log = list(map(json.loads, separated_log[1:]))
-                for regex, keyword in expression:
-                    if any([regex.match(failure['test']) for failure in separated_log]):
-                        matches[keyword].append(job_name)
+
             else:
-                continue
+                if url.endswith('live_backing.log'):
+                    unexpected_failures = [line for line in separated_log if 'TEST-UNEXPECTED-FAIL' in line]
+                    for regex, keyword in expression:
+                        if any([regex.match(failure) for failure in unexpected_failures]):
+                            matches[keyword].append(job_name)
+                elif url.endswith('errorsummary.log'):
+                    separated_log = list(map(json.loads, separated_log[1:]))
+                    for regex, keyword in expression:
+                        if any([regex.match(failure['test']) for failure in separated_log]):
+                            matches[keyword].append(job_name)
+                else:
+                    continue
 
-    if matches:
-        print('Failures found:')
-        for key in matches.keys():
-            print('-->', key)
-            list(map(print, matches[key]))
-            print('\n')
+    finish_time = round(time.perf_counter() - start_time, 5)
 
-    finish_time = round(time.perf_counter() - api_query_time, 5)
     print(f'Finished processing task logs in {finish_time} seconds.')
+    return matches
+
+
+def process_results(matches, revision):
+    print(f'Keywords found in f{revision}:')
+    for key in matches.keys():
+        print('-->', key)
+        if matches[key]:
+            list(map(print, matches[key]))
+        else:
+            print('None found')
+        print('\n')
 
 
 def main():
     args = parse_arguments(sys.argv[1:])
-    process_logs(args)
+    log_urls = get_list_of_log_urls(args.branch, args.revision)
+    matches = process_logs(log_urls, args.keywords, args.ran)
+    process_results(matches, args.revision)
 
 if __name__ == '__main__':
     main()
